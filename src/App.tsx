5176c8afdebdd485323a791dfda70e5982e58c44
mengdaoshizhongxinyang <programmercyk@gmail.com>
2023-09-27 23:54:56 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { defineComponent, ref } from "vue";
 
 export default defineComponent({
+  props:{
+    a:{
+      type:String
+    }
+  },
   setup(props, ctx) {
     const res=ref('')
     return ()=><input></input>
